f9ed2380ee1bc1ca4a8d93e116d4f5b3223cbb0c
Christopher Lübbemeier <luebbemeier@metaventis.com>
2023-10-28 18:51:57 +0200

    Hide back button to root

--- src/routes/[token]/[...path]/+layout.svelte
+++ src/routes/[token]/[...path]/+layout.svelte
@@ -3,9 +3,17 @@
 	import { backTarget } from '$lib/stores';
 	import { getUrlPath } from '$lib/utils';
 	import type { RouteParams } from './$types';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
 
 	$: path = getUrlPath($page.params as RouteParams);
-	$: backTarget.set(`/${path.split('/').slice(0, -1).join('/')}`);
+	// Don't show the back button to the root directory for users without upload token.
+	$: if (data.hasUploadToken || path.split('/').length > 1) {
+		backTarget.set(`/${path.split('/').slice(0, -1).join('/')}`);
+	} else {
+		backTarget.set(null);
+	}
 </script>
 
 <slot />
