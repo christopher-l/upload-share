92ecffb296d67673cf871bf1f4122f119f26864e
Christopher Lübbemeier <luebbemeier@metaventis.com>
2023-10-28 18:30:37 +0200

    Upload token

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,20 @@
+import { env } from '$env/dynamic/private';
 import { listRootDir, type FileEntry } from '$lib/server/filesystem';
+import { error } from '@sveltejs/kit';
+import { parse } from 'url';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad<{ fileList: FileEntry[] }> = async ({ params }) => {
-	return { fileList: await listRootDir() };
+export const load: PageServerLoad<{ fileList: FileEntry[] }> = async ({ cookies, request }) => {
+	if (!env.UPLOAD_TOKEN) {
+		throw error(403);
+	}
+	if (cookies.get('token') === env.UPLOAD_TOKEN) {
+		return { fileList: await listRootDir() };
+	}
+	const url = parse(request.url, true);
+	if (url.query.token === env.UPLOAD_TOKEN) {
+		cookies.set('token', url.query.token);
+		return { fileList: await listRootDir() };
+	}
+	throw error(403);
 };
