f0aaef913948a0874e2c1e4b67c883b5ab5d2224
Christopher Lübbemeier <luebbemeier@metaventis.com>
2023-10-30 13:41:23 +0100

    New folder, delete in sub dir

--- src/routes/[token]/[...path]/+page.server.ts
+++ src/routes/[token]/[...path]/+page.server.ts
@@ -1,7 +1,15 @@
-import { getEntryType, listSubDir, type FileEntry } from '$lib/server/filesystem';
+import { hasValidUploadToken } from '$lib/server/authentication';
+import {
+	createFolder,
+	getEntryType,
+	listSubDir,
+	remove,
+	type FileEntry
+} from '$lib/server/filesystem';
 import { getDownloadHref, getFilePath } from '$lib/server/utils';
 import { getUrlPath } from '$lib/utils';
 import { error, redirect } from '@sveltejs/kit';
+import { join } from 'path';
 import type { PageServerLoad } from './$types';
 
 export interface FileListEntry extends FileEntry {
@@ -37,5 +45,56 @@ export const load: PageServerLoad<{ fileList: FileListEntry[] }> = async ({ para
 			throw redirect(302, `/${getUrlPath(params)}/preview`);
 		default:
 			throw error(404);
+	}
+};
+
+export const actions = {
+	newFolder: async ({ cookies, request, params }) => {
+		if (!hasValidUploadToken(request, cookies)) {
+			throw error(401);
+		}
+		const data = await request.formData();
+		const name = data.get('name')?.toString().trim();
+		if (!name || name.includes('/')) {
+			throw 400;
+		}
+		const path = await getFilePath(params);
+		if (!path) {
+			throw error(404);
+		}
+		const newFolderPath = join(path, name);
+		try {
+			await createFolder(newFolderPath);
+		} catch (e) {
+			if (e.code === 'EEXIST') {
+				throw error(409);
+			} else {
+				throw error(500);
+			}
+		}
+	},
+	delete: async ({ cookies, request, params }) => {
+		if (!hasValidUploadToken(request, cookies)) {
+			throw error(401);
+		}
+		const data = await request.formData();
+		const name = data.get('name')?.toString().trim();
+		if (!name || name.includes('/')) {
+			throw 400;
+		}
+		const path = await getFilePath(params);
+		if (!path) {
+			throw error(404);
+		}
+		const deletePath = join(path, name);
+		try {
+			await remove(deletePath);
+		} catch (e) {
+			if (e.code === 'ENOENT') {
+				throw error(404);
+			} else {
+				throw error(500);
+			}
+		}
 	}
 };
