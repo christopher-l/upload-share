eac8743c5fab345bfd41ef68e805f108bd1432a3
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2023-12-20 12:30:50 +0100

    Revert "Mobile layout for top bar"
    This reverts commit 9f31a2ca9ebd489a69f475c4f4109f72ef54e986.

--- src/lib/TopBar.svelte
+++ src/lib/TopBar.svelte
@@ -3,23 +3,19 @@
 </script>
 
 <header>
-	{#if $backTarget || $downloadTarget}
-		<div class="left">
-			<a class="icon standard" href={$backTarget}>
-				<iconify-icon icon="mdi:arrow-left" width="36" height="36" />
-			</a>
-		</div>
-	{/if}
+	<div class="left">
+		<a class="icon standard" href={$backTarget}>
+			<iconify-icon icon="mdi:arrow-left" width="36" height="36" />
+		</a>
+	</div>
 	<div class="center container">
 		<slot />
 	</div>
-	{#if $backTarget || $downloadTarget}
-		<div class="right">
-			<a class="icon standard" href={$downloadTarget} download>
-				<iconify-icon icon="mdi:download" width="36" height="36" />
-			</a>
-		</div>
-	{/if}
+	<div class="right">
+		<a class="icon standard" href={$downloadTarget} download>
+			<iconify-icon icon="mdi:download" width="36" height="36" />
+		</a>
+	</div>
 </header>
 
 <style lang="scss">
@@ -44,23 +40,5 @@
 	}
 	a:not([href]) {
 		visibility: hidden;
-	}
-	@media (max-width: 419.98px) {
-		header {
-			display: grid;
-		}
-		.left,
-		.right {
-			grid-row: 1 / 2;
-		}
-		.left {
-			justify-self: start;
-		}
-		.right {
-			justify-self: end;
-		}
-		.center {
-			grid-column: 1 / 3;
-		}
 	}
 </style>
